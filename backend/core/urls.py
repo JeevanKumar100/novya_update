e2dd074e29f4bd7f863b6b26a5028a6e6daf5922
srikrishna206 <165254384+srikrishna206@users.noreply.github.com>
2025-12-03 15:30:01 +0530

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,6 +5,7 @@ from .views import health, favicon, get_classes
 urlpatterns = [
     path('', health, name='health'),
     path('favicon.ico', favicon, name='favicon'),
+    path('api/classes/', get_classes, name='get_classes'),
 ]
 
 
